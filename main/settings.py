3bff5421cdd6a63e6b4592c1693b339f0b4a3848
DVDphobia <l1gend@outlook.com>
2025-12-22 13:41:31 +0700

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-275$ol_&fs-fylqh(b)w0n!dja3p$l9mc@(o97r*g90eopk85u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', 'localhost', '127.0.0.1']
 
 
 # Application definition
